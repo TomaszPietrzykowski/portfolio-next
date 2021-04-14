e0c1899775a41eaaa82c340302c3627d6242b31a
TomaszPietrzykowski <pietrzykowski77@gmail.com>
2021-04-14 17:54:45 +0200

    hero rework

--- pages/index.js
+++ pages/index.js
@@ -29,13 +29,7 @@ function HomePage() {
                   href="https://github.com/TomaszPietrzykowski"
                   target="_blank"
                 >
-                  <svg
-                    width="32"
-                    height="32"
-                    viewBox="0 0 32 32"
-                    fill="none"
-                    xmlns="http://www.w3.org/2000/svg"
-                  >
+                  <svg width="32" height="32" viewBox="0 0 32 32" fill="none">
                     <path
                       className={styles.path}
                       fill-rule="evenodd"
@@ -51,6 +45,7 @@ function HomePage() {
         </div>
       </header>
       <main className={styles.hero}>
+        <div className={styles.heroImgLeft}></div>
         <div className={styles.heroContainer}>
           <h2 className={styles.subtitle}>Javascript Development</h2>
           <h1 className={styles.title}>
@@ -60,6 +55,12 @@ function HomePage() {
           <button className={styles.cta}>My Work</button>
           <div className={styles.heroImg}></div>
         </div>
+        <svg id="wave" viewBox="0 0 1440 300" className={styles.svg}>
+          <path
+            fill="white"
+            d="M0,96L80,101.3C160,107,320,117,480,144C640,171,800,213,960,202.7C1120,192,1280,128,1360,96L1440,64L1440,320L1360,320C1280,320,1120,320,960,320C800,320,640,320,480,320C320,320,160,320,80,320L0,320Z"
+          ></path>
+        </svg>
       </main>
       <section className={styles.featured}>
         <div className={styles.container}></div>
