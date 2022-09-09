0977ab92ad4f368c7b32592a0300caafe6e8065f
TomaszPietrzykowski <pietrzykowski77@gmail.com>
2022-09-09 22:35:47 +0200

    project page added

---
+++ pages/projects/climatemonitor.js
@@ -0,0 +1,24 @@
+import styles from '../styles/Featured.module.css'
+
+const ClimateMonitor = () => {
+  return (
+    // rethink classes
+    <main className={styles.detailsContainer}>
+      <h1>Project title</h1>
+      <section className={styles.detailsEven}>
+        <div className={styles.detailsLeft}></div>
+        <div className={styles.detailsRight}></div>
+      </section>
+      <section className={styles.detailsUneven}>
+        <div className={styles.detailsLeft}></div>
+        <div className={styles.detailsRight}></div>
+      </section>
+      <section className={styles.detailsEven}>
+        <div className={styles.detailsLeft}></div>
+        <div className={styles.detailsRight}></div>
+      </section>
+    </main>
+  )
+}
+
+export default ClimateMonitor
