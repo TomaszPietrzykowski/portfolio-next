f476ecd2b67f560a21b0eb4bb10b72f80e3e236c
TomaszPietrzykowski <pietrzykowski77@gmail.com>
2022-09-11 13:18:25 +0200

    climate monitor project details page drafted

--- pages/projects/climatemonitor.js
+++ pages/projects/climatemonitor.js
@@ -1,16 +1,56 @@
-import styles from '../styles/Featured.module.css'
+import styles from '../../styles/Projects.module.css'
+import Link from 'next/link'
 
 const ClimateMonitor = () => {
   return (
     // rethink classes
-    <main>
-      <h1>Project title</h1>
-      <section className={styles.detailsEven}>
-        <div className={styles.detailsLeft}></div>
-        <div className={styles.detailsRight}></div>
-      </section>
+    <main className={styles.detailsContainer}>
       <section className={styles.detailsUneven}>
-        <div className={styles.detailsLeft}></div>
+        <div className={styles.detailsLeft}>
+          <div className={styles.detailsContent}>
+            <h2 className={styles.detailsSubtitle}>Public REST API</h2>
+            <h1 className={styles.detailsTitle}>Climate Monitor</h1>
+            <h3 className={styles.detailsWww}>www.climatemonitor.info</h3>
+            <h4 className={styles.detailsSubhead}>Description</h4>
+            <p className={styles.detailsPara}>
+              Free public API serving json data on climate change. Well over 40
+              endpoints with data on main climate factors. Data from NASA, NOAA
+              and Berkely Institute, stored, updated daily and served on demand
+              without limits. Frontend of the app provides neat charts with
+              dynamic scope for data analysis, climate newsfeed with headline
+              from last 30 days and documentation of the API.
+            </p>
+            <h4 className={styles.detailsSubhead}>Quick links</h4>
+            <p className={styles.detailsPara}>
+              <ul>
+                <li>
+                  <a href='https://climatemonitor.info' target='_blank'>
+                    Project live
+                  </a>
+                </li>
+                <li>
+                  <a
+                    href='https://github.com/TomaszPietrzykowski/climate-monitor-nextjs'
+                    target='_blank'
+                  >
+                    Repo frontend
+                  </a>
+                </li>
+                <li>
+                  <a
+                    href='https://github.com/TomaszPietrzykowski/climate-monitor'
+                    target='_blank'
+                  >
+                    Repo backend
+                  </a>
+                </li>
+                <li>
+                  <Link href='/projects'>Back to all projects</Link>
+                </li>
+              </ul>
+            </p>
+          </div>
+        </div>
         <div className={styles.detailsRight}></div>
       </section>
       <section className={styles.detailsEven}>
